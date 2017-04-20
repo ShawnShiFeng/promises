cd9c26397ecb7c54e2144107e4879fd13d95d9d8
Hack Reactor Student <student@hackreactor.com>
2017-04-20 12:41:28 -0700

    promise repo complete

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,37 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var promisification = require('./promisification');
+var promiseConstructor = require('./promiseConstructor');
 
+
+
+var writeResponseToFilePath = function(body, writeFilePath) {
+	
+	return new Promise(function (resolve, reject) {
+		
+		fs.writeFile(writeFilePath,body,'utf8', function(err) {
+			if (err) {
+				reject(err);
+			} else {
+				console.log("Function 3 done");
+				
+				var profile = fs.readFileSync(writeFilePath, 'utf8');
+				console.log("hi" + profile);
+				resolve();
+			}
+		})
+	})
+}
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  	
+	return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath)
+		.then(promisification.getGitHubProfileAsync)
+			.then(function(body){
+				writeResponseToFilePath (body, writeFilePath)
+			});
 };
 
 // Export these functions so we can test them
